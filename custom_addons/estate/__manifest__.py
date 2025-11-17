6bcd77104fc2e9fc11b0dcabe36aeb8427c25a73
bkyw <buckcode16@gmail.com>
2025-11-17 11:24:47 +0900

    feat(security): add model access rights

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -4,9 +4,8 @@
     'depends': ['base'],
     'author': "Author Name",
     'category': 'Category',
-    'description': """
-    Description text
-    """,
+    'description': """Description text""",
     'data': [
+        'security/ir.model.access.csv',
     ],
 }
