3f3377edd9c50fffbaf138b236c508823052764d
bkyw <buckcode16@gmail.com>
2025-11-20 15:43:11 +0900

    feat(res_users): add domain to relationship

--- custom_addons/estate/models/inherited_res_users.py
+++ custom_addons/estate/models/inherited_res_users.py
@@ -5,5 +5,8 @@ class ResUsers(models.Model):
     _inherit = ["res.users"]
 
     property_ids = fields.One2many(
-        "estate.property", "salesperson_id", string="Properties"
+        "estate.property",
+        "salesperson_id",
+        string="Properties",
+        domain=[("state", "in", ["new", "offer_received"])],
     )
