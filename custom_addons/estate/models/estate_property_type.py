aaab860b66d6347e27926a64b6970599e88a97bb
bkyw <buckcode16@gmail.com>
2025-11-20 12:39:09 +0900

    feat(views): add stat button on the property type form view which shows the list of all offers related to properties of the given type when it is clicked on

--- custom_addons/estate/models/estate_property_type.py
+++ custom_addons/estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
@@ -13,5 +13,12 @@ class EstatePropertyType(models.Model):
 
     property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
     sequence = fields.Integer('Sequence', default=1, help="Used to property types.")
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id", string="Property offers")
+    offer_count = fields.Integer(compute="_compute_offer_count", string = "Offer counts")
     
     name = fields.Char(required=True)
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
