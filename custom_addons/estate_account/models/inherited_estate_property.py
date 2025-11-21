2e17a3b5e6ef532a4a107c796f503dcf94b08e68
bkyw <buckcode16@gmail.com>
2025-11-21 16:07:43 +0900

    feat(estate_account): add invoice lines

--- custom_addons/estate_account/models/inherited_estate_property.py
+++ custom_addons/estate_account/models/inherited_estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import Command, models
 from odoo.exceptions import UserError
 
 
@@ -9,7 +9,26 @@ class InheritedEstateProperty(models.Model):
         for record in self:
             partner_id = record.buyer_id.id
 
-            invoice_vals = {"partner_id": partner_id, "move_type": "out_invoice"}
+            invoice_vals = {
+                "partner_id": partner_id,
+                "move_type": "out_invoice",
+                "invoice_line_ids": [
+                    Command.create(
+                        {
+                            "name": "commission",
+                            "quantity": 1,
+                            "price_unit": record.selling_price * 0.06,
+                        }
+                    ),
+                    Command.create(
+                        {
+                            "name": "administrative fees",
+                            "quantity": 1,
+                            "price_unit": 100,
+                        }
+                    ),
+                ],
+            }
 
             self.env["account.move"].sudo().create(invoice_vals)
 
